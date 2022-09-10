bc92ce14fa3f296ce4099371379444e39d681daf
Asif Mulla <asifmulla166037@gmail.com>
2022-09-10 16:02:43 +0530

    css utilies fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
-// import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    // provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     AngularFireModule.initializeApp(environment.firebaseConfig), // initialize the angular firebase
   ],
   providers: [],
